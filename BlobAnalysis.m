feb1d074fbe6a7f6efb8415c84834013b62dc782
Orth <robbey.orth@perkinswill.com>
2016-08-10 11:54:30 -0500

    Combo'd files and rewrote PW_Vision adding in basic Kalman tracking

--- BlobAnalysis.m
+++ BlobAnalysis.m
@@ -49,7 +49,7 @@ while ~isDone(video)
    frame = step(video);
    mask = foregroundDetector.step(frame);
    
-% Morphological Image Cleaning
+%% Morphological Image Cleaning
    
 %    cleanMask = imopen(mask, strel('Disk',1));
    cleanMask = imopen(mask, strel('rectangle', [3,3]));
@@ -58,7 +58,7 @@ while ~isDone(video)
    
 %    flow = estimateFlow(opticFlow, cleanMask);
 
-% Blob detection
+%% Blob detection
    [centroids, bboxes] = blobs.step(cleanMask);
    
 %    [peoples, scores] = detectPeopleACF(frame,...
@@ -67,7 +67,7 @@ while ~isDone(video)
    
    if ~isempty(bboxes)
 
-%Filter detections based off of size, aspect ratio, location
+%% Filter detections based off of size, aspect ratio, location
 %         [bboxDogs, centDogs] = detectionFilter(dogRatio, 400, bboxes, centroids);
         [bboxPeople, centPeople] = detectionFilter(ROI, peopleRatio, 1500, bboxes, centroids);
 %    frame = insertShape(frame, 'rectangle', bboxDogs, 'Color', 'green');
@@ -76,9 +76,23 @@ while ~isDone(video)
    
 
 
-% Predict track Locations
+%% Predict track Locations
         trackPredictions(tracks);
 
+%% Assign Detections to Tracks
+        nTracks = length(tracks);
+        nDetections = size(centroids, 1);
+        
+        % Compute the cost of assigning each detection to each track.
+        cost = zeros(nTracks, nDetections);
+        for i = 1:nTracks
+            cost(i, :) = distance(tracks(i).kalmanFilter, centroids);
+        end
+        
+        % Solve the assignment problem.
+        costOfNonAssignment = 20;
+        [assignments, unassignedTracks, unassignedDetections] = ...
+            assignDetectionsToTracks(cost, costOfNonAssignment);
 
 
 
@@ -88,9 +102,88 @@ while ~isDone(video)
 %    hold off;
    end
    
+   %% Update assigned tracks
+   numAssignedTracks = size(assignments, 1);
+        for i = 1:numAssignedTracks
+            trackIdx = assignments(i, 1);
+            detectionIdx = assignments(i, 2);
+            centroid = centroids(detectionIdx, :);
+            bbox = bboxes(detectionIdx, :);
+            
+            % Correct the estimate of the object's location
+            % using the new detection.
+            correct(tracks(trackIdx).kalmanFilter, centroid);
+            
+            % Replace predicted bounding box with detected
+            % bounding box.
+            tracks(trackIdx).bbox = bbox;
+            
+            % Update track's age.
+            tracks(trackIdx).age = tracks(trackIdx).age + 1;
+            
+            % Update visibility.
+            tracks(trackIdx).totalVisibleCount = ...
+                tracks(trackIdx).totalVisibleCount + 1;
+            tracks(trackIdx).consecutiveInvisibleCount = 0;
+        end
    
+   %% update Unassigned Tracks
+   for i = 1:length(unassignedTracks)
+            ind = unassignedTracks(i);
+            tracks(ind).age = tracks(ind).age + 1;
+            tracks(ind).consecutiveInvisibleCount = ...
+                tracks(ind).consecutiveInvisibleCount + 1;
+   end
    
+   %% delete tracks
+   if isempty(tracks)
+            return;
+        end
+        
+        invisibleForTooLong = 20;
+        ageThreshold = 8;
+        
+        % Compute the fraction of the track's age for which it was visible.
+        ages = [tracks(:).age];
+        totalVisibleCounts = [tracks(:).totalVisibleCount];
+        visibility = totalVisibleCounts ./ ages;
+        
+        % Find the indices of 'lost' tracks.
+        lostInds = (ages < ageThreshold & visibility < 0.6) | ...
+            [tracks(:).consecutiveInvisibleCount] >= invisibleForTooLong;
+        
+        % Delete lost tracks.
+        tracks = tracks(~lostInds);
    
+   %% Create New Tracks
+   
+   centroids = centroids(unassignedDetections, :);
+        bboxes = bboxes(unassignedDetections, :);
+        
+        for i = 1:size(centroids, 1)
+            
+            centroid = centroids(i,:);
+            bbox = bboxes(i, :);
+            
+            % Create a Kalman filter object.
+            kalmanFilter = configureKalmanFilter('ConstantVelocity', ...
+                centroid, [50, 200], [25, 250], 500);
+            
+            % Create a new track.
+            newTrack = struct(...
+                'id', nextId, ...
+                'bbox', bbox, ...
+                'kalmanFilter', kalmanFilter, ...
+                'age', 1, ...
+                'totalVisibleCount', 1, ...
+                'consecutiveInvisibleCount', 0);
+            
+            % Add it to the array of tracks.
+            tracks(end + 1) = newTrack;
+            
+            % Increment the next id.
+            nextId = nextId + 1;
+        end
 %Continue to next frame
    step(vPlayer, frame);
    step(fPlayer, cleanMask);
